3ee4cf43c1e2a661f2ddf753e57bc6bea086f782
Jonas Tilman <tillman.jonas@gmail.com>
2021-01-04 14:53:22 +0800

    Start refactoring

---
+++ data_read_in.R
@@ -0,0 +1,108 @@
+# Read In Secrets
+# path <- "./secrets.json"
+# if (!file.exists(path)) {
+#     stop("Can't find secret file: '", path, "'")
+# }
+# secrets = jsonlite::read_json(path)
+
+
+df_from_db = function(){
+  ##############
+  # Respondents
+  respondentdf <- mongo("respondents", url = "mongodb://127.0.0.1:27017/thesis-dev")$find()
+  respondentdf[,ncol(respondentdf)] <- NULL
+  glimpse(respondentdf)
+  
+  ##############
+  # Responses
+  responsesdf <- mongo("responses", url = "mongodb://127.0.0.1:27017/thesis-dev")$find()
+  responsesdf[,ncol(responsesdf)] <- NULL
+  
+  # Variable generation
+  responsesdf$maptype = as.numeric(stringr::str_extract(responsesdf$mapVersion, "/[0-9]+") %>% str_remove("/") )
+  responsesdf$colour = stringr::str_extract(responsesdf$mapVersion, "green|blue|red")
+  responsesdf$legend = stringr::str_extract(responsesdf$mapVersion, "sampled|annotated|checkered|headline|clustered")
+  
+  responsesdf$inputChanges = lapply(responsesdf$sliderChanges, function(x){ length(x$val) }) %>% unlist()
+  responsesdf$submitVal = lapply(responsesdf$sliderChanges, function(x){ tail(x$val, 1) }) %>% unlist()
+  responsesdf$rangeMin = lapply(responsesdf$sliderChanges, function(x){ min(x$val) }) %>% unlist()
+  responsesdf$rangeMax = lapply(responsesdf$sliderChanges, function(x){ max(x$val) }) %>% unlist()
+  
+  # INCLUDE WHICH MAP
+  # imagehover events
+  hoverev_list = responsesdf %>% 
+    apply(MARGIN = 1,function(x){ 
+      if(ncol(x$imageHoverEvents)){
+        data.frame(
+          "uuid" = x$uuid,
+          "mapVersion" = x$mapVersion,
+          "timestamp" = x$imageHoverEvents$timestamp,
+          "x" = x$imageHoverEvents$x,
+          "y" = x$imageHoverEvents$x
+        )
+      }
+    })
+  hover_df = do.call("rbind", hoverev_list)
+  
+  # Slider events as separate DF
+  sliderev_list = responsesdf %>% 
+    apply(MARGIN = 1,function(x){ 
+      if(ncol(x$sliderChanges)){
+        data.frame(
+          "uuid" = x$uuid,
+          "mapVersion" = x$mapVersion,
+          "timestamp" = x$sliderChanges$timestamp,
+          "val" = x$sliderChanges$val
+        )
+      }
+    })
+  slider_df = do.call("rbind", sliderev_list)
+  
+  
+  
+  # Prog-number of user
+  prog_df_list = respondentdf %>% 
+    apply(MARGIN = 1,function(x){ 
+      data.frame(
+        "uuid" = x$uuid,
+        "imageProg" = x$imageProg,
+        "prog" = 1:length(x$imageProg)
+      )
+    })
+  progressiondf <- do.call("rbind", prog_df_list)
+  
+  ##############
+  # Acceptance
+  acceptancessdf <- mongo("acceptances", url = "mongodb://127.0.0.1:27017/thesis-dev")$find()
+  acceptancessdf[,ncol(acceptancessdf)] <- NULL
+  acc_cols_to_num <- c(1:5)                                  
+  acceptancessdf[ , acc_cols_to_num] <- apply(acceptancessdf[ , acc_cols_to_num], 2,            # Specify own function within apply
+                                              function(x) as.numeric(x))
+  acceptancessdf_long = acceptancessdf %>% tidyr::gather("legend", "acceptance", -uuid)
+  
+  ##############
+  # Actual values
+  correct = read.csv("./data/correct-vals.csv")
+  
+  
+  # Joins
+  responses_joined = left_join(responsesdf, correct, by = c("maptype" ="MapVersion"))
+  responses_joined$percept_error = responses_joined$submitVal - responses_joined$correctVal
+  responses_joined$percept_error_abs = abs(responses_joined$percept_error)
+  
+  responses_joined = left_join(
+    responses_joined, acceptancessdf_long, 
+    c("uuid", "legend"), 
+    keep=F
+  )
+  
+  responses_joined = left_join(
+    responses_joined, progressiondf, 
+    c("uuid", "mapVersion" = "imageProg"), 
+    keep=F
+  )
+  
+  clean_df = responses_joined %>% select(-c(mapVersion, sliderChanges, imageHoverEvents)) 
+  
+  return(clean_df)
+}
--- modeling_init.R
+++ modeling_init.R
@@ -3,116 +3,57 @@ library(stringr)
 library(dplyr)
 library(ggplot2)
 
+# RES QUESTIONS
+# DONE: - How is data opacity mapping perception influenced by legend display choices?
+# DONE: (maybe have checkered as baseline) - Does alternative legend methods compared to GIS industry standard’s choice influence errors in perceived data mapping and colour choices?
+# DONE: - Does legend to data proximity-reduction or contextualised legend design influence perception errors for opacity data mapping?
+# DONE: - Can decision times and user selection behaviour be affected by legend choices?
+# DONE: - How are alternative legend design choices received by the subjects (subjective user acceptance testing)?
+# ADD: colour differences?
+
+# R studio wd same as source file
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+
 ########################################
 # Data READ IN
 ########################################
-respondents <- mongo("respondents", url = "mongodb://127.0.0.1:27017/thesis-dev")
-responses <- mongo("responses", url = "mongodb://127.0.0.1:27017/thesis-dev")
-acceptances <- mongo("acceptances", url = "mongodb://127.0.0.1:27017/thesis-dev")
+source("data_read_in.R")
 
-
-correct = read.csv("~/Dev/Thesis/thesis-data-analysis/correct-vals.csv")
-names(correct) <- c("MapVersion", "correctVal")
-
-
-respondentdf = respondents$find()
-responsesdf = responses$find()
-acceptancessdf = acceptances$find()
-acceptancessdf[,ncol(acceptancessdf)] <- NULL
-acc_cols_to_num <- c(1:5)                                  
-acceptancessdf[ , acc_cols_to_num] <- apply(acceptancessdf[ , acc_cols_to_num], 2,            # Specify own function within apply
-                    function(x) as.numeric(x))
-
-acceptancessdf_long = acceptancessdf %>% tidyr::gather("legend", "acceptance", -uuid)
-
-
-names(responsesdf) <- c("uuid", "mapVersion", "sliderChanges", "imageHoverEvents", "submitTime", "TBR")
-
+responses_an = df_from_db()
+# Read from CSV as alternative
+# readr::write_csv(responses_an, "./data/responses.csv")
 
 ########################################
 # Data Cleaning
 ########################################
-
-
-########################################
-# Variable generation
-########################################
-
-responsesdf$maptype = as.numeric(stringr::str_extract(responsesdf$mapVersion, "/[0-9]+") %>% str_remove("/") )
-responsesdf$colour = stringr::str_extract(responsesdf$mapVersion, "green|blue|red")
-responsesdf$legend = stringr::str_extract(responsesdf$mapVersion, "sampled|annotated|checkered|headline|clustered")
-
-responsesdf$inputChanges = lapply(responsesdf$sliderChanges, function(x){ length(x$val) }) %>% unlist()
-responsesdf$submitVal = lapply(responsesdf$sliderChanges, function(x){ tail(x$val, 1) }) %>% unlist()
-responsesdf$rangeMin = lapply(responsesdf$sliderChanges, function(x){ min(x$val) }) %>% unlist()
-responsesdf$rangeMax = lapply(responsesdf$sliderChanges, function(x){ max(x$val) }) %>% unlist()
-
-# TODO: imagehover events
-
-
-# Prog-number of user
-prog_df_list = respondentdf %>% 
-  apply(MARGIN = 1,function(x){ 
-    data.frame(
-      "uuid" = x$uuid,
-      "imageProg" = x$imageProg,
-      "prog" = 1:length(x$imageProg)
-    )
-  })
-progressiondf <- do.call("rbind", prog_df_list)
-progressiondf %>% glimpse()
-
-
-
-
-
-responsesd_joined = left_join(responsesdf, correct, by = c("maptype" ="MapVersion"))
-responsesd_joined$percept_error = responsesd_joined$submitVal - responsesd_joined$correctVal
-responsesd_joined$percept_error_abs = abs(responsesd_joined$percept_error)
-
-responsesd_joined = left_join(
-  responsesd_joined, acceptancessdf_long, 
-  c("uuid", "legend"), 
-  keep=F
-)
-
-responsesd_joined = left_join(
-  responsesd_joined, progressiondf, 
-  c("uuid", "mapVersion" = "imageProg"), 
-  keep=F
-)
-
-
-responsesd_joined$uuid = as.factor(responsesd_joined$uuid)
-responsesd_joined$mapVersion = as.factor(responsesd_joined$mapVersion)
+responses_an$uuid = as.factor(responses_an$uuid)
+responses_an$maptype = as.factor(responses_an$maptype)
+responses_an$colour = as.factor(responses_an$colour)
+responses_an$legend = as.factor(responses_an$legend)
+# TODO: if needed remove outliers
 
 ########################################
 # Summary statistics/Exploratory analysis
 ########################################
-responses_an = responsesd_joined %>% select(-c(mapVersion, sliderChanges, imageHoverEvents, TBR)) 
 summary(responses_an)
+# TODO: When color perception -> plot against error - should be correlated if not random...
 
 # TODO: Increase models (all FE-models)
 # TODO: statistical assumptions section (export as functions...)
-# TODO: When color perception -> plot against error - should be correlated if not random...
+# TODO: colourvalueerror - once there is alpha-picker data
 # TODO: Look at response time as % of first image time... learning/remember/tired effects?
 # TODO: learning effects as progression or getting tired of task... (INDIVIDUAL+PROGNUM)
 # TODO: Progression fixed effects or as linear effect?
 # TODO: Coefficient plots
 # TODO: More plots
-# TODO: colourvalueerror - once there is alpha-picker data
-# TODO: acceptance scores analysis
-# TODO: SelectionRagneDistFromCorrectAnswer
-# TODO: Subset = first 4-5 images? if signs of learning/remember/tired effects
-# TODO-OPTIONAL: Colour map of interactions
+
 
 ########################################
 # Visual inspection and outliers by variables
-# TODO: if needed remove outliers
 # 1-way ANOVA + plots
 # TODO: Check statistical assumptions BEFORE running models
 # https://stat-methods.com/home/one-way-anova-r/#:~:text=%20One-way%20ANOVA%20Annotated%20R%20Output%20%201,the%20theoretical%20normal%20reference%20line%20and...%20More%20
-# http://www.sthda.com/english/wiki/one-way-anova-test-in-r
 # http://www.sthda.com/english/wiki/one-way-anova-test-in-r
 ########################################
 
@@ -156,13 +97,14 @@ summary(m)
 
 
 # Acceptance
+# How are alternative legend design choices received by the subjects 
+# (subjective user acceptance testing)?
 p <- ggplot(responses_an, aes(factor(legend), acceptance))
 p + geom_violin() + geom_jitter(height = 0, width = 0.1, alpha=0.5)
 p + geom_boxplot() + geom_jitter(height = 0, width = 0.1, alpha=0.5)
 
 m <-aov(acceptance ~ factor(legend), data=responses_an)
 summary(m)
-
 
 
 
@@ -252,13 +194,10 @@ coeftest(fe_mod, vcov. = vcovHC, type = "HC1")
 
 
 
-# 3) Learning-effects not included if no specific progression is 
-# fixed for the subjects, this is assumed to eliminate any such effects. 
-# If clear learning effects are included – but no immediate feedback 
-# to the subjects during the study ought to reduce those effects 
-# slightly.
-
-
 library(stargazer)
 stargazer(lm_mod, fe_mod, title="Regression Results", align=TRUE)
 
+
+# TODO: Subset = first 4-5 images? if signs of learning/remember/tired effects
+# TODO-OPTIONAL: SelectionRagneDistFromCorrectAnswer
+# TODO-OPTIONAL: Colour map of interactions + interaction map of users (changed inputs, etc..)
