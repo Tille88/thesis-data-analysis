22e3881baff1d5e6899f41990dfa68e5475fc73b
Jonas Tillman <tillman.jonas@gmail.com>
2021-03-16 14:46:59 +0800

    Data read-in from commited .RData file

--- data_read_in.R
+++ data_read_in.R
@@ -9,119 +9,119 @@
 
 df_from_db = function(){
   
-  db_string = "mongodb://127.0.0.1:27017/thesis-loc-prod"
-  
-  ##############
-  # Respondents
-  respondentdf <- mongo("respondents", url = db_string)$find()
-  respondentdf[,ncol(respondentdf)] <- NULL
-  
-  
-  ##############
-  # Responses
-  responsesdf <- mongo("responses", url = db_string)$find()
-  responsesdf[,ncol(responsesdf)] <- NULL
-  
-  # Variable generation
-  responsesdf$maptype = as.numeric(stringr::str_extract(responsesdf$mapVersion, "/[0-9]+") %>% str_remove("/") )
-  responsesdf$colour = stringr::str_extract(responsesdf$mapVersion, "green|blue|red")
-  responsesdf$legend = stringr::str_extract(responsesdf$mapVersion, "sampled|annotated|checkered|headline|clustered")
-  
-  responsesdf$inputChanges = lapply(responsesdf$sliderChanges, function(x){ length(x$val) }) %>% unlist()
-  responsesdf$submitVal = lapply(responsesdf$sliderChanges, function(x){ tail(x$val, 1) }) %>% unlist()
-  responsesdf$rangeMin = lapply(responsesdf$sliderChanges, function(x){ min(x$val) }) %>% unlist()
-  responsesdf$rangeMax = lapply(responsesdf$sliderChanges, function(x){ max(x$val) }) %>% unlist()
-
-  responsesdf$hoverEvents = lapply(responsesdf$imageHoverEvents, function(x){ nrow(x) }) %>% unlist()
-  # INCLUDE WHICH MAP
-  # imagehover events
-  hoverev_list = responsesdf %>% 
-    apply(MARGIN = 1,function(x){ 
-      if(ncol(x$imageHoverEvents)){
-        data.frame(
-          "uuid" = x$uuid,
-          "mapVersion" = x$mapVersion,
-          "timestamp" = x$imageHoverEvents$timestamp,
-          "x" = x$imageHoverEvents$x,
-          "y" = x$imageHoverEvents$y
-        )
-      }
-    })
-  hover_df = do.call("rbind", hoverev_list)
-  
-  # Slider events as separate DF
-  sliderev_list = responsesdf %>% 
-    apply(MARGIN = 1,function(x){ 
-      if(ncol(x$sliderChanges)){
-        data.frame(
-          "uuid" = x$uuid,
-          "mapVersion" = x$mapVersion,
-          "timestamp" = x$sliderChanges$timestamp,
-          "val" = x$sliderChanges$val
-        )
-      }
-    })
-  slider_df = do.call("rbind", sliderev_list)
-  
-  
-  
-  # Prog-number of user
-  prog_df_list = respondentdf %>% 
-    apply(MARGIN = 1,function(x){ 
-      data.frame(
-        "uuid" = x$uuid,
-        "imageProg" = x$imageProg,
-        "prog" = 1:length(x$imageProg)
-      )
-    })
-  progressiondf <- do.call("rbind", prog_df_list)
-  
-  ##############
-  # Acceptance
-  acceptancessdf <- mongo("acceptances", url = db_string)$find()
-  acceptancessdf[,ncol(acceptancessdf)] <- NULL
-  acc_cols_to_num <- c(1:5)                                  
-  acceptancessdf[ , acc_cols_to_num] <- apply(acceptancessdf[ , acc_cols_to_num], 2,            # Specify own function within apply
-                                              function(x) as.numeric(x))
-  acceptancessdf_long = acceptancessdf %>% tidyr::gather("legend", "acceptance", -uuid)
-  
-  ##############
-  # Actual values
-  correct = read.csv("../data/correct-vals.csv")
-  
-  
-  # Joins
-  responses_joined = left_join(responsesdf, correct, by = c("maptype" ="MapVersion"))
-  responses_joined$percept_error = responses_joined$submitVal - responses_joined$correctVal
-  responses_joined$percept_error_abs = abs(responses_joined$percept_error)
-  
-  responses_joined = left_join(
-    responses_joined, acceptancessdf_long, 
-    c("uuid", "legend"), 
-    keep=F
-  )
-  
-  responses_joined = left_join(
-    responses_joined, progressiondf, 
-    c("uuid", "mapVersion" = "imageProg"), 
-    keep=F
-  )
-  
-  clean_df = responses_joined %>% select(-c(mapVersion, sliderChanges, imageHoverEvents)) 
-  
-  save(
-    respondentdf, 
-    responsesdf, 
-    acceptancessdf, 
-    hover_df, 
-    slider_df, 
-    progressiondf, 
-    acceptancessdf_long, 
-    clean_df,
-    file = paste0("../data/", Sys.time() %>% str_replace_all("[ :]", "-"), ".RData")
-  )
-  # load("data.RData") used to programmatically read in file
-  
+  # db_string = "mongodb://127.0.0.1:27017/thesis-loc-prod"
+  # 
+  # ##############
+  # # Respondents
+  # respondentdf <- mongo("respondents", url = db_string)$find()
+  # respondentdf[,ncol(respondentdf)] <- NULL
+  # 
+  # 
+  # ##############
+  # # Responses
+  # responsesdf <- mongo("responses", url = db_string)$find()
+  # responsesdf[,ncol(responsesdf)] <- NULL
+  # 
+  # # Variable generation
+  # responsesdf$maptype = as.numeric(stringr::str_extract(responsesdf$mapVersion, "/[0-9]+") %>% str_remove("/") )
+  # responsesdf$colour = stringr::str_extract(responsesdf$mapVersion, "green|blue|red")
+  # responsesdf$legend = stringr::str_extract(responsesdf$mapVersion, "sampled|annotated|checkered|headline|clustered")
+  # 
+  # responsesdf$inputChanges = lapply(responsesdf$sliderChanges, function(x){ length(x$val) }) %>% unlist()
+  # responsesdf$submitVal = lapply(responsesdf$sliderChanges, function(x){ tail(x$val, 1) }) %>% unlist()
+  # responsesdf$rangeMin = lapply(responsesdf$sliderChanges, function(x){ min(x$val) }) %>% unlist()
+  # responsesdf$rangeMax = lapply(responsesdf$sliderChanges, function(x){ max(x$val) }) %>% unlist()
+  # 
+  # responsesdf$hoverEvents = lapply(responsesdf$imageHoverEvents, function(x){ nrow(x) }) %>% unlist()
+  # # INCLUDE WHICH MAP
+  # # imagehover events
+  # hoverev_list = responsesdf %>% 
+  #   apply(MARGIN = 1,function(x){ 
+  #     if(ncol(x$imageHoverEvents)){
+  #       data.frame(
+  #         "uuid" = x$uuid,
+  #         "mapVersion" = x$mapVersion,
+  #         "timestamp" = x$imageHoverEvents$timestamp,
+  #         "x" = x$imageHoverEvents$x,
+  #         "y" = x$imageHoverEvents$y
+  #       )
+  #     }
+  #   })
+  # hover_df = do.call("rbind", hoverev_list)
+  # 
+  # # Slider events as separate DF
+  # sliderev_list = responsesdf %>% 
+  #   apply(MARGIN = 1,function(x){ 
+  #     if(ncol(x$sliderChanges)){
+  #       data.frame(
+  #         "uuid" = x$uuid,
+  #         "mapVersion" = x$mapVersion,
+  #         "timestamp" = x$sliderChanges$timestamp,
+  #         "val" = x$sliderChanges$val
+  #       )
+  #     }
+  #   })
+  # slider_df = do.call("rbind", sliderev_list)
+  # 
+  # 
+  # 
+  # # Prog-number of user
+  # prog_df_list = respondentdf %>% 
+  #   apply(MARGIN = 1,function(x){ 
+  #     data.frame(
+  #       "uuid" = x$uuid,
+  #       "imageProg" = x$imageProg,
+  #       "prog" = 1:length(x$imageProg)
+  #     )
+  #   })
+  # progressiondf <- do.call("rbind", prog_df_list)
+  # 
+  # ##############
+  # # Acceptance
+  # acceptancessdf <- mongo("acceptances", url = db_string)$find()
+  # acceptancessdf[,ncol(acceptancessdf)] <- NULL
+  # acc_cols_to_num <- c(1:5)                                  
+  # acceptancessdf[ , acc_cols_to_num] <- apply(acceptancessdf[ , acc_cols_to_num], 2,            # Specify own function within apply
+  #                                             function(x) as.numeric(x))
+  # acceptancessdf_long = acceptancessdf %>% tidyr::gather("legend", "acceptance", -uuid)
+  # 
+  # ##############
+  # # Actual values
+  # correct = read.csv("../data/correct-vals.csv")
+  # 
+  # 
+  # # Joins
+  # responses_joined = left_join(responsesdf, correct, by = c("maptype" ="MapVersion"))
+  # responses_joined$percept_error = responses_joined$submitVal - responses_joined$correctVal
+  # responses_joined$percept_error_abs = abs(responses_joined$percept_error)
+  # 
+  # responses_joined = left_join(
+  #   responses_joined, acceptancessdf_long, 
+  #   c("uuid", "legend"), 
+  #   keep=F
+  # )
+  # 
+  # responses_joined = left_join(
+  #   responses_joined, progressiondf, 
+  #   c("uuid", "mapVersion" = "imageProg"), 
+  #   keep=F
+  # )
+  # 
+  # clean_df = responses_joined %>% select(-c(mapVersion, sliderChanges, imageHoverEvents)) 
+  # 
+  # save(
+  #   respondentdf, 
+  #   responsesdf, 
+  #   acceptancessdf, 
+  #   hover_df, 
+  #   slider_df, 
+  #   progressiondf, 
+  #   acceptancessdf_long, 
+  #   clean_df,
+  #   file = paste0("../data/", Sys.time() %>% str_replace_all("[ :]", "-"), ".RData")
+  # )
+  # load("./2021-02-06-11-45-27.RData") #used to programmatically read in file
+  load(paste0("../data/", "2021-02-06-11-45-27.RData"))
   
   return(clean_df)
 }
